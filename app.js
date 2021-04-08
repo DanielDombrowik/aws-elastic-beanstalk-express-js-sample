dfcf535be76b591c6a4e7dba34c1ddc15caed932
ddombrowik <Daniel.Dombrowik@cartus.com>
2021-04-08 11:38:37 -0400

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World,  it has been a slice!'));
+app.get('/', (req, res) => res.send('Hello World,  this is actually fun!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
